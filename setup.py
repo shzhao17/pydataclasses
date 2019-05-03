8c1f683c2a37181e95db4d249511ee8028f47ef7
Patrick Zhao <>
2019-05-03 17:29:53 +0800

    Bump the version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst', 'r', 'utf-8') as f:
 
 setup(
     name='pydataclasses',
-    version='1.0.0',
+    version='1.0.1',
     description='Python Data Classes',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
